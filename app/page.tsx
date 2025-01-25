71eca8f6aafce4c4b3a84cbe17ce9049cda15aa0
Snehasish Mandal <mandalsneh97@gmail.com>
2025-01-25 21:47:39 +0530

    Updated the Project Config

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Go</Button>
+      Developed by GetSetDeployed
+    </div>
+  );
+}
