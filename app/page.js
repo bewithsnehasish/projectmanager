4565957f5d61187f733a4e8071ab584c7b28435e
Snehasish Mandal <mandalsneh97@gmail.com>
2025-02-08 04:24:01 +0530

    Updated the Project Manager

--- app/page.js
+++ app/page.js
@@ -17,39 +17,8 @@ import {
 } from "@/components/ui/accordion";
 import CompanyCarousel from "@/components/company-carousel";
 import Image from "next/image";
-
-const faqs = [
-  {
-    question: "What is ZCRUM?",
-    answer:
-      "ZCRUM is a powerful project management tool designed to help teams organize, track, and manage their work efficiently. It combines intuitive design with robust features to streamline your workflow and boost productivity.",
-  },
-  {
-    question: "How does ZCRUM compare to other project management tools?",
-    answer:
-      "ZCRUM offers a unique combination of intuitive design, powerful features, and flexibility. Unlike other tools, we focus on providing a seamless experience for both agile and traditional project management methodologies, making it versatile for various team structures and project types.",
-  },
-  {
-    question: "Is ZCRUM suitable for small teams?",
-    answer:
-      "Absolutely! ZCRUM is designed to be scalable and flexible. It works great for small teams and can easily grow with your organization as it expands. Our user-friendly interface ensures that teams of any size can quickly adapt and start benefiting from ZCRUM's features.",
-  },
-  {
-    question: "What key features does ZCRUM offer?",
-    answer:
-      "ZCRUM provides a range of powerful features including intuitive Kanban boards for visualizing workflow, robust sprint planning tools for agile teams, comprehensive reporting for data-driven decisions, customizable workflows, time tracking, and team collaboration tools. These features work seamlessly together to enhance your project management experience.",
-  },
-  {
-    question: "Can ZCRUM handle multiple projects simultaneously?",
-    answer:
-      "Yes, ZCRUM is built to manage multiple projects concurrently. You can easily switch between projects, and get a bird's-eye view of all your ongoing work. This makes ZCRUM ideal for organizations juggling multiple projects or clients.",
-  },
-  {
-    question: "Is there a learning curve for new users?",
-    answer:
-      "While ZCRUM is packed with features, we've designed it with user-friendliness in mind. New users can quickly get up to speed thanks to our intuitive interface, helpful onboarding process, and comprehensive documentation.",
-  },
-];
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import faqs from "@/data/faqs.json";
 
 const features = [
   {
@@ -72,11 +41,41 @@ const features = [
   },
 ];
 
+// HeroScrollDemo Component
+function HeroScrollDemo() {
+  return (
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold  gradient-title">
+              Unleash the Power of GetSetDeployed Project Manager
+              <br />
+              <span className="text-4xl md:text-[6rem] font-bold leading-none">
+                Made for Agile Teams
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <Image
+          src={`/bg.png`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className="min-h-screen">
-      {/* Hero Section */}
-      <section className="container mx-auto py-20 text-center">
+      {/* Hero Section 
+      <section className="container mx-auto pt-20 text-center">
         <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
           Streamline Your Workflow <br />
           <span className="flex mx-auto gap-3 sm:gap-4 items-center">
@@ -93,7 +92,7 @@ export default function Home() {
         <p className="text-xl text-gray-300 mb-10 max-w-3xl mx-auto">
           Empower your team with our intuitive project management solution.
         </p>
-        <p className="text-xl mb-12 max-w-2xl mx-auto"></p>
+        <p className="text-xl max-w-2xl mx-auto"></p>
         <Link href="/onboarding">
           <Button size="lg" className="mr-4">
             Get Started <ChevronRight size={18} className="ml-1" />
@@ -105,6 +104,20 @@ export default function Home() {
           </Button>
         </Link>
       </section>
+      */}
+      {/* Hero Section */}
+      <section className=" mx-auto pt-20 text-center">
+        <Image
+          src={"/getsetdeployed2.png"}
+          alt="Zscrum Logo"
+          width={600} // Increased width
+          height={120} // Increased height
+          className="mx-auto w-auto object-contain" // Centered and responsive width
+        />
+      </section>
+
+      {/* Hero Scroll Animation Section */}
+      <HeroScrollDemo />
 
       {/* Features Section */}
       <section id="features" className="bg-gray-900 py-20 px-5">
